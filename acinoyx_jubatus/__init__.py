9028b0981ffcdaf53a9112178f1abf2135507c6a
Karina <komana@uninorte.edu.co>
2021-10-24 15:09:42 -0500

    add images to database

--- acinoyx_jubatus/__init__.py
+++ acinoyx_jubatus/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 def crear_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '1ab2c3d4e5f6g7'
+    app.config['UPLOAD_FOLDER']="static/images"
 
     from .views import views
     from .auth import auth
@@ -12,12 +13,13 @@ def crear_app():
     app.register_blueprint(auth, url_prefix='/')
 
 
-    #conn = sql.connect('database.db')
-    #print("exitoso")
-    #conn.execute('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, profile_photo BLOB, email TEXT, user_name TEXT, password TEXT, age INT )')
-    #conn.execute('CREATE TABLE posts (id INTEGER PRIMARY KEY AUTOINCREMENT, photo BLOB, foot_note TEXT, post_date TEXT, user_id INT, FOREIGN KEY(user_id) REFERENCES users(id))')
-    #conn.execute('CREATE TABLE comments (id INTEGER PRIMARY KEY AUTOINCREMENT, post_id INT, comment TEXT, user_id INT, date TEXT, like INT, FOREIGN KEY(user_id) REFERENCES users(id) , FOREIGN KEY(post_id) REFERENCES posts(id))')
-    #conn.close()
+    conn = sql.connect('database.db')
+    conn.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, profile_photo TEXT, email TEXT, user_name TEXT, password TEXT, age INT, rol INT)')
+    conn.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, photo TEXT, foot_note TEXT, post_date TEXT, user_id INT, FOREIGN KEY(user_id) REFERENCES users(id))')
+    conn.execute('CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY AUTOINCREMENT, post_id INT, comment TEXT, user_id INT, date TEXT, like INT, FOREIGN KEY(user_id) REFERENCES users(id) , FOREIGN KEY(post_id) REFERENCES posts(id))')
+    conn.execute('CREATE TABLE IF NOT EXISTS roles (id INTEGER, rol TEXT)')
+    
+    conn.close()
 
 
     return app
--- acinoyx_jubatus/views.py
+++ acinoyx_jubatus/views.py
@@ -1,9 +1,13 @@
 #Aqui se definen y se guardan las rutas estandar de la pagina (a donde los usuarios pueden ir)
+import os
 import sqlite3 as sql
-from flask import Blueprint, render_template, request, flash  #Estos importes permiten tener este archivo como un plano para la pagina (Blueprint), a renderizar las vistas html
+import datetime
+from flask import Blueprint, render_template, request, flash, redirect, current_app
+from flask.helpers import url_for  #Estos importes permiten tener este archivo como un plano para la pagina (Blueprint), a renderizar las vistas html
                                                               #A pedir informacion (request) y mostrar mensajes en la pantalla (flash)
 
 views = Blueprint('views', __name__)    #Asi es como se definira el "plano" (Blueprint)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 @views.route('/', methods=['GET', 'POST'])
 def login():
@@ -11,7 +15,41 @@ def login():
 
 @views.route('/feed', methods=['GET', 'POST'])
 def feed():
-    return render_template("feed.html")
+
+    # Data for tests
+    #posts = []
+    #post1 = {
+    #    'user_id': 'Karina Omana',
+    #    'foot_note': 'Mi primer post',
+    #    'photo': "La foto"
+    #}
+    #post2 = {
+    #    'user_id': 'Mariale',
+    #    'foot_note': 'Mi segundo post',
+    #    'photo': "La foto"
+    #}
+    #post3 = {
+    #    'user_id': 'Eduardo',
+    #    'foot_note': 'Mi tercer post',
+    #}
+    #posts.append(post1)
+    #posts.append(post2)
+    #posts.append(post3)
+    #data = posts
+
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute(
+        """
+        SELECT t1.photo, t1.foot_note, t1.post_date, t2.user_name
+        FROM posts t1
+        LEFT JOIN users t2 on t1.user_id = t2.id 
+        """
+    )
+    data = cur.fetchall()
+    con.close()
+
+    return render_template("feed.html", posts=data)
 
 @views.route('/resultados_busqueda', methods=['GET', 'POST'])
 def resultados_busqueda():
@@ -38,9 +76,7 @@ def registro():
                 cur.execute('INSERT INTO users (user_name, email, age, password) VALUES (?,?,?,?)',(name,email,age, password))
                 con.commit()
                 flash("registro exitoso")
-                return render_template("feed.html")
-
-
+                return redirect(url_for("views.feed"))
 
     return render_template("registro.html")
 
@@ -59,5 +95,26 @@ def detalle_post_2():
 
 @views.route('/post', methods=['GET', 'POST'])
 def post():
+
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_user = 1
+        post_date = datetime.datetime.now()
+        upload_image = request.files['upload_image']
+        if upload_image.filename != '':
+            filepath = os.path.join(basedir, current_app.config['UPLOAD_FOLDER'], upload_image.filename)
+            upload_image.save(filepath)
+        
+        con = sql.connect('database.db')
+        cur = con.cursor()
+        cur.execute("INSERT INTO posts(photo, foot_note, post_date, user_id) VALUES(?,?,?,?)",
+            (upload_image.filename, post_title, post_date, post_user)
+        )
+        con.commit()
+        flash("Publicacion creada!")
+
+        return redirect(url_for("views.feed"))
+
+
     return render_template("crear_post.html")
 
